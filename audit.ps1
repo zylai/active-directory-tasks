2a7771549b572770603ca305a504b96238300096
zylai <40312846+zylai@users.noreply.github.com>
2021-03-30 20:48:48 -0700

    Update audit.ps1

--- audit.ps1
+++ audit.ps1
@@ -1,14 +1,14 @@
 # Check if Msol cmdlet is installed
 if (-Not (Get-Command -Module MSOnline -errorAction SilentlyContinue)) {
-    echo "Msol is not installed. This script requires the Msol cmdlet to work"
-    echo 'To install this module, run the following command in PowerShell as administrator (select "[A] Yes to All" when prompted):'
-    echo ""
-    echo "Install-Module MSOnline"
-    echo ""
-    echo "More info: https://docs.microsoft.com/en-us/microsoft-365/enterprise/connect-to-microsoft-365-powershell?view=o365-worldwide"
-    echo ""
-    Read-Host -Prompt "Press Enter to exit"
-    exit
+	echo "Msol is not installed. This script requires the Msol cmdlet to work"
+	echo 'To install this module, run the following command in PowerShell as administrator (select "[A] Yes to All" when prompted):'
+	echo ""
+	echo "Install-Module MSOnline"
+	echo ""
+	echo "More info: https://docs.microsoft.com/en-us/microsoft-365/enterprise/connect-to-microsoft-365-powershell?view=o365-worldwide"
+	echo ""
+	Read-Host -Prompt "Press Enter to exit"
+	exit
 }
 
 echo "Please enter your credentials when prompted"
@@ -17,19 +17,19 @@ echo "Please enter your credentials when prompted"
 $authentication_status = $False
 while (-not $authentication_status)
 {
-    $UserCredential = $host.ui.PromptForCredential("Credentials needed", "Use your UPN for the username.", "", "UPN")
-    Connect-MsolService -Credential $UserCredential -errorAction SilentlyContinue
+	$UserCredential = $host.ui.PromptForCredential("Credentials needed", "Use your UPN for the username.", "", "UPN")
+	Connect-MsolService -Credential $UserCredential -errorAction SilentlyContinue
 
-    $authentication_status = $?
+	$authentication_status = $?
 
-    if ($authentication_status)
-    {
-        echo "Authentication successful"
-    } 
-    else
-    {
-        echo "Authentication failed, try again"
-    }
+	if ($authentication_status)
+	{
+		echo "Authentication successful"
+	} 
+	else
+	{
+		echo "Authentication failed, try again"
+	}
 }
 
 # Instruction messages
@@ -50,28 +50,28 @@ $num_lines = $(echo $guids | Measure-Object -Line | Select Lines -ExpandProperty
 # Loop through each line in $guids and get UPN of users with the role
 foreach($line in $($guids -split "`r`n"))
 {
-    Write-Progress -activity "Gathering data..." -status "Processed: $progress_bar of $num_lines roles" -percentComplete (($progress_bar / $num_lines)  * 100)
+	Write-Progress -activity "Gathering data..." -status "Processed: $progress_bar of $num_lines roles" -percentComplete (($progress_bar / $num_lines)  * 100)
 
-    # Only get email address if member type is a user and save in $result
-    $result = $(Get-MsolRoleMember -RoleObjectId $line | Select RoleMemberType, EmailAddress -ExpandProperty EmailAddress)
+	# Only get email address if member type is a user and save in $result
+	$result = $(Get-MsolRoleMember -RoleObjectId $line | Select RoleMemberType, EmailAddress -ExpandProperty EmailAddress)
 
-    # Count number of lines in $result and if it's zero, that means the role does not have any users
-    # In that case, save the name of the role in empty-roles.txt
-    $num_users = $(echo $result | Measure-Object -Line | Select Lines -ExpandProperty Lines)
-    if ($num_users -eq 0)
-    {
-        Get-MsolRole -ObjectId "$line" | Select Name -ExpandProperty Name >> ([Environment]::GetFolderPath("Desktop")+"\empty-roles-$date.txt")
-        $progress_bar++
-        continue
-    }
+	# Count number of lines in $result and if it's zero, that means the role does not have any users
+	# In that case, save the name of the role in empty-roles.txt
+	$num_users = $(echo $result | Measure-Object -Line | Select Lines -ExpandProperty Lines)
+	if ($num_users -eq 0)
+	{
+		Get-MsolRole -ObjectId "$line" | Select Name -ExpandProperty Name >> ([Environment]::GetFolderPath("Desktop")+"\empty-roles-$date.txt")
+		$progress_bar++
+		continue
+	}
 
-    # Assuming the above if-statement is false, print name of the role and list of users
-    Get-MsolRole -ObjectId "$line" | Select Name -ExpandProperty Name >> ([Environment]::GetFolderPath("Desktop")+"\audit-$date.txt")
-    echo "---" >> ([Environment]::GetFolderPath("Desktop")+"\audit-$date.txt")
-    echo $result >> ([Environment]::GetFolderPath("Desktop")+"\audit-$date.txt")
-    echo "" >> ([Environment]::GetFolderPath("Desktop")+"\audit-$date.txt")
+	# Assuming the above if-statement is false, print name of the role and list of users
+	Get-MsolRole -ObjectId "$line" | Select Name -ExpandProperty Name >> ([Environment]::GetFolderPath("Desktop")+"\audit-$date.txt")
+	echo "---" >> ([Environment]::GetFolderPath("Desktop")+"\audit-$date.txt")
+	echo $result >> ([Environment]::GetFolderPath("Desktop")+"\audit-$date.txt")
+	echo "" >> ([Environment]::GetFolderPath("Desktop")+"\audit-$date.txt")
 
-    $progress_bar++
+	$progress_bar++
 }
 
 # Print list of roles with no users by pasting empty-roles.txt back in
