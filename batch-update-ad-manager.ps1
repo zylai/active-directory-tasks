c5f53a82564433067b1b258469966aeb9c77a67b
zylai <40312846+zylai@users.noreply.github.com>
2021-04-13 20:32:42 -0700

    Update and rename batch-update-ad-manager-field.ps1 to batch-update-ad-manager.ps1

--- batch-update-ad-manager.ps1
+++ batch-update-ad-manager.ps1
@@ -18,17 +18,27 @@ $SelfListedAsManager = $False
 foreach ($Employee in $DirectReports)
 {
 	$EmployeeUsername = $(($Employee | Select SamAccountName -ExpandProperty SamAccountName | Out-String).Trim())
+	$EmployeeName = $(($Employee | Select Name -ExpandProperty Name | Out-String).Trim())
 	Get-ADUser -Filter {SamAccountName -eq $EmployeeUsername} | Set-ADUser -Credential $Credential -Manager $NewManagerObject
+
+	echo "Updated manager for $EmployeeName"
 
 	if ($EmployeeUsername -eq $NewManager)
 	{
 		$SelfListedAsManager = $True
-		$SelfListedAsManagerUserObject = $Employee
+		$SelfListedAsManagerObject = $Employee
 	}
 }
 
 if ($SelfListedAsManager)
 {
-	echo "It seems like ___ was promoted from within the department and is listed as their own manager. Enter _____'s new manager. If ____ does not report to anyone, leave blank and hit Enter"
-	$SelfListedAsManagerUserObject
+	$SelfListedAsManagerUsername = $(($SelfListedAsManagerObject | Select SamAccountName -ExpandProperty SamAccountName | Out-String).Trim())
+	$SelfListedAsManagerName = $(($SelfListedAsManagerObject | Select Name -ExpandProperty Name | Out-String).Trim())
+
+	$SelfListedEmployeeNewManagerUsername = Read-Host "It seems like $SelfListedAsManagerName ($SelfListedAsManagerUsername) was promoted from within the department and is now listed as their own manager. Enter $SelfListedAsManagerName's new manager. If $SelfListedAsManagerName does not report to anyone, leave blank and hit Enter"
+	$SelfListedEmployeeNewManagerObject = $(Get-ADUser -Filter {SamAccountName -eq $SelfListedEmployeeNewManagerUsername})
+
+	Get-ADUser -Filter {SamAccountName -eq $SelfListedAsManagerUsername} | Set-ADUser -Credential $Credential -Manager $SelfListedEmployeeNewManagerObject
+
+	echo "Updated manager for $SelfListedAsManagerName"
 }
